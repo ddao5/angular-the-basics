6b4334bbd4aa6a47f902d3fc1bef1e189db06694
Duc Dao <43461796+ddao5@users.noreply.github.com>
2021-06-03 01:45:40 -0400

    Finished Databinding and its assignment!

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = 'Testserver';
+  username: string = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+
+  onCreateServer(): void {
+    this.serverCreationStatus = `Server was created! Name is ${this.serverName}.`;
+  }
+  onClickButtonUserName(): void {
+    this.username = '';
+  }
+  onUpdateServerName(event: Event): void {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }

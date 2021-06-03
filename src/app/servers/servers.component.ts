214d85111f88aa2d46fb25495a4b0c9a66ec55b6
Duc Dao <43461796+ddao5@users.noreply.github.com>
2021-06-03 02:36:43 -0400

    Finished Learning Basics of Directives and its Assignment!

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,11 @@ export class ServersComponent implements OnInit {
   serverCreationStatus: string = 'No server was created!';
   serverName: string = 'Testserver';
   username: string = '';
+  serverCreated: boolean = false;
+  servers = ['Testserver', 'Testserver2'];
+  secretContent: string = 'Yo what\'s up';
+  shouldDisplay: boolean = false;
+  displayLogs = [];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -23,9 +28,15 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(): void {
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}.`;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
   }
   onClickButtonUserName(): void {
     this.username = '';
+  }
+  onClickDisplayDetails(): void {
+    this.shouldDisplay = !this.shouldDisplay;
+    this.displayLogs.push(Date.now());
   }
   onUpdateServerName(event: Event): void {
     this.serverName = (<HTMLInputElement>event.target).value;
